3b1ac38f9d8185f3216fdaa777fcd1587fbd79d1
Ankur <borkar.1@iitj.ac.in>
2022-05-28 11:30:09 +0530

    env file

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const searchRoutes = require("./routes/search");
 const profileRoutes = require("./routes/profile");
 
 const req = require("express/lib/request");
-const MONGODB_URI = "mongodb+srv://anku_13:Pass%401234@cluster0.gmbvrut.mongodb.net/campplace?retryWrites=true&w=majority"
+const MONGODB_URI = process.env.DATABASE;
 
 
 const store = new MongoDBStore({uri: MONGODB_URI, collection: 'sessions'})
